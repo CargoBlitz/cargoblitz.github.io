da9576a9ff1344b0c40a8399c4251e1c83dee247
CargoBlitz <maithemauro2@gmail.com>
2024-05-04 15:31:47 +0200

    Mobile nav and desktop nav DONE!

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 const hamburger = document.querySelector('.hamburger-menu input');
 const sidebar = document.querySelector('.sidebar');
-const liElements = document.getElementById("li-mobile")
+const liElements = document.querySelectorAll('.li-mobile');
 
 hamburger.addEventListener('change', () => {
     if (hamburger.checked) {
+        liElements.forEach(li => {
+            li.style.display = "block";
+            li.style.animation = 'fadeIn 0.2s';
+          });
         sidebar.classList.add('open');
         sidebar.style.display = 'flex';
         sidebar.style.animation = 'show 0.3s forwards';
     } else {
         sidebar.style.animation = 'hide 0.3s forwards';
+        liElements.forEach(li => {
+            li.style.animation = 'fadeOut 0.2s';
+            setTimeout(() => {
+                li.style.display = "none";
+              }, 150)
+          });
         setTimeout(() => {
             sidebar.style.display = 'none';
             sidebar.classList.remove('open');
